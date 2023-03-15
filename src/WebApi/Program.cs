af32ca1e1db0397f5999f8f06c40c82a61a2f7c7
personal <shaneel_kumar@live.com>
2023-03-15 15:07:02 +1300

    Initial commit of React (Vite) & ASP WebAPI integrated template

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Http.Headers;
+using Microsoft.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSpaStaticFiles(configuration =>
+{
+    configuration.RootPath = "ClientApp/dist";
+});
 
 var app = builder.Build();
 
@@ -22,5 +29,44 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+var spaPath = "/app";
+if (app.Environment.IsDevelopment())
+{
+    app.MapWhen(y => y.Request.Path.StartsWithSegments(spaPath), client =>
+    {
+        client.UseSpa(spa =>
+        {
+            spa.UseProxyToSpaDevelopmentServer("https://localhost:6363");
+        });
+    });
+}
+else
+{
+    app.Map(new PathString(spaPath), client =>
+        {
+            client.UseSpaStaticFiles();
+            client.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "clientapp";
+
+                // adds no-store header to index page to prevent deployment issues (prevent linking to old .js files)
+                // .js and other static resources are still cached by the browser
+                spa.Options.DefaultPageStaticFileOptions = new StaticFileOptions
+                {
+                    OnPrepareResponse = ctx =>
+                    {
+                        ResponseHeaders headers = ctx.Context.Response.GetTypedHeaders();
+                        headers.CacheControl = new CacheControlHeaderValue
+                        {
+                            NoCache = true,
+                            NoStore = true,
+                            MustRevalidate = true
+                        };
+                    }
+                };
+            });
+        });
+}
 
 app.Run();
